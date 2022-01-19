6b93ad71c83bb6019213514945451b93d6b87b79
SamanthaDison <samnicdis@gmail.com>
2022-01-18 23:23:53 -0700

    first commit

---
+++ VUE-NASA/src/pages/APODPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row mt-2 justify-content-center">
+    <div class="col-10">
+      <form @submit.prevent="searchAPOD()" action="">
+        <div class="form-group d-flex">
+          <input type="date" class="form-control" v-model="editable" />
+          <button btn btn-secondary>Search</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { apodService } from "../services/ApodService";
+import { ref } from "@vue/reactivity";
+import Pop from "../utils/Pop";
+export default {
+  setup() {
+    const editable = ref("");
+
+    return {
+      async searchAPOD() {
+        try {
+          await apodService.searchAPOD(editable.value);
+        } catch (error) {
+          Pop(error.message, "error");
+        }
+      },
+      editable,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ VUE-NASA/src/services/ApodService.js
@@ -0,0 +1,17 @@
+import Axios from 'axios'
+
+const apiKey = 'cz18VhedCEfiZKibPcUm3Qa69o6WMKFvJ2Dg72M2'
+
+const nasaApi = Axios.create({
+    baseURL: 'https://api.nasa.gov/planetary'
+})
+
+class APODService {
+    async searchAPOD(searchDate) {
+        const res = await nasaApi(`apod?query=${searchDate}&api_key=${apiKey}`)
+        console.log('testing', res)
+    }
+
+}
+export const apodService = new APODService()
+
